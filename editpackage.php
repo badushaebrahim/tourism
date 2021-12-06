02acaa7450ecda5df745ed6a751e894347724b28
unknown <bbadusha13@gmail.com>
2021-12-06 12:46:54 +0530

    added mysql

--- editpackage.php
+++ editpackage.php
@@ -23,11 +23,10 @@ if(($mok=mysqli_query($conn,$pk))&&($pp=mysqli_query($conn,$sk))){
 	
 	echo"
 <tr><td>Location:</td><td> <textarea  name='Loc' >$ro[location]</textarea></td></tr><br><br><br><br>
-
+<tr><td>About:</td><td> <textarea name='About' >$ro[about]</textarea></td></tr><br><br><br><br>
 <tr><td>Cost: </td><td><input type='number' name='Cost' value='$ro[cost]' ></td></tr><br><br><br>
 <tr><td>sec1</td><td><textarea name='sec1'>$rom[sec1]</textarea></td></tr>
-
-";
+<tr><td>sec2</td><td><textarea name='sec2'>$rom[sec2]</textarea></td></tr>";
 echo"<tr><td></td><td><input type='Submit' name='sub' value='Submit'><a href='home2.php'><input type='button' name='sub' value='Back'></a></td></tr>
 
 </form>
@@ -43,20 +42,20 @@ echo"<tr><td></td><td><input type='Submit' name='sub' value='Submit'><a href='ho
 
 if(isset($_POST['sub'])){
 $cost=$_POST['Cost'];
-//$about=$_POST['About'];
+$about=$_POST['About'];
 $loc=$_POST['Loc'];
 $sec1=$_POST['sec1'];
-//$sec2=$_POST['sec2'];
+$sec2=$_POST['sec2'];
 
 echo "<script>console.log('inside45')</script>";
 //$about22="UPDATE `aboutpack` SET `sec1`='$sec1',`sec2`='$sec2',`location`='$loc' WHERE `packid`=$id"
-//$packs="UPDATE `packages` SET`cost`=$cost,`about`=$about,`location`=$loc WHERE `pack_no`=$id ";
+$packs="UPDATE `packages` SET`cost`=$cost,`about`=$about,`location`=$loc WHERE `pack_no`=$id ";
 //echo "<script>console.log($cost+$about+$loc+$sec1)</script>";
 //$packs="UPDATE `packages` SET `cost`=$cost,`about`='$about',`location`='$loc' WHERE `pack_no`=$id";
-//echo "<script>console.log('$about')</script>";
+echo "<script>console.log('$sec2')</script>";
 
 //$about2="UPDATE `aboutpack` SET `sec1`='$sec1,`sec2`=$sec2,`location`= $loc WHERE `packid`=$id";
-$about2="UPDATE `aboutpack` SET `sec1`='$sec1',  `location`='$loc' WHERE `packid`='$id;";
+$about2="UPDATE `aboutpack` SET `sec1`='$sec1', `sec2`='$sec2', `location`='$loc' WHERE `packid`='$id;";
 if($mok=mysqli_query($conn,$about2)){
 	echo "<script>console.log('yo')</script>";
 	if($pp=mysqli_query($conn,$about2)){
